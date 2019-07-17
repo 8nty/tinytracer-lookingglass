d319dc45dc61481bf8b184a4b88d385b26d812e0
Ave Ozkal <ave@ave.zone>
2019-07-17 14:10:21 +0300

    Get basic API done

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Small looking glass tool, designed for IPv6 but can be used for IPv4 too.
 
 Powers lg for AS208590 at https://lg.lasagna.dev/
 
+## Dedicated to
+
+This project is dedicated to Heather Heyer. You are not forgotten, you will not be forgotten.
+
--- tinytracer.py
+++ tinytracer.py
@@ -10,21 +10,39 @@ with open("config.json") as f:
 
 def check_ip(ip):
     # TODO: do actual regexes here
-    return True
+    if "." in ip:
+        return 4
+    elif ":" in ip:
+        return 6
+    else:
+        return False
 
 
 @app.route('/api/task/<string:task_name>/<string:ip>')
 def do_mtr(task_name, ip):
-    print(f"IP: {ip}")
+    ip_type = check_ip(ip)
+
+    if not ip_type:
+        response = {"success": False, "reason": "Invalid IP address"}
+        return jsonify(response)
+
     if task_name not in config["tasks"]:
         response = {"success": False, "reason": "No such task found"}
         return jsonify(response)
-    # mtr -6zboLDRSNBAWVGJMXI -a 2a0d:1a40:7700:: -rwc10 -i 0.2 -m 50 2a0d:1a40:1337::
-    # traceroute -A -q5 -w 2 -6 -m 50 2a0d:1a40:1337::
-    # ping6 -c 10 -i 0.2 -t 255  2a0d:1a40:1337::
-    subprocess.run(# the command)
-        # return output here
-    response = {"success": True}
+    
+    if f"cmd{ip_type}" not in config["tasks"][task_name]:
+        response = {"success": False,
+                    "reason": f"This task doesn't support IPv{ip_type}"}
+        return jsonify(response)
+    
+    if not config["capabilities"][f"ipv{ip_type}"]:
+        response = {"success": False,
+                    "reason": f"This server doesn't support IPv{ip_type}"}
+        return jsonify(response)
+
+    cmd = config["tasks"][task_name][f"cmd{ip_type}"].replace("{{TARGET}}", ip)
+    cmd_out = subprocess.run(cmd, shell=True, capture_output=True)
+    response = {"success": True, "result": cmd_out.stdout.decode()}
     return jsonify(response)
 
 if __name__ == "__main__":
